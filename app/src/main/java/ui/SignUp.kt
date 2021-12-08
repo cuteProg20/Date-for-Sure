cb7b5b000d4580bb61948e63de8cc4927080e148
cuteProg20 <janeyjackytz@gmail.com>
2021-12-08 17:18:16 +0300

    Sign Up Interface design

---
+++ app/src/main/java/ui/SignUp.kt
@@ -0,0 +1,12 @@
+package ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dateforsure.R
+
+class SignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_up)
+    }
+}
