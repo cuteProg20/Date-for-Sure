3b5dc035865a7ccab742e332f196860ca13213ba
cuteProg20 <janeyjackytz@gmail.com>
2021-12-08 19:26:26 +0300

    Sign Up Interface design collection

--- app/src/main/java/ui/MainActivity.kt
+++ app/src/main/java/ui/MainActivity.kt
@@ -1,12 +1,19 @@
 package ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.dateforsure.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun gotoSign(v: View) {
+        val intent = Intent(this, SignUp::class.java)
+        startActivity(intent)
     }
 }
