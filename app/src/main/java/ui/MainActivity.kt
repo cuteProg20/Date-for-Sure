8f91cf9dca1c3565736ebf3bfe1409cde9fdd90c
cuteprogrammer <janeyjackytz@gmail.com>
2021-12-28 17:50:58 +0300

    Form Validation

--- app/src/main/java/ui/MainActivity.kt
+++ app/src/main/java/ui/MainActivity.kt
@@ -1,25 +1,141 @@
 package ui
 
 import android.content.Intent
+import android.util.Patterns
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dateforsure.R
 import com.example.dateforsure.databinding.ActivityMainBinding
-import kotlinx.android.synthetic.main.activity_main.*
+import org.intellij.lang.annotations.Pattern
 
 
 class MainActivity : AppCompatActivity() {
 
+
+    private lateinit var binding: ActivityMainBinding
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        
+       binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        usernameFocusChanger()
+        passwordFocusListener()
+
+        binding.buttonlogin.setOnClickListener { submitForm() }
 
     }
+
+    private fun submitForm() {
+
+        binding.usernamecontainer.helperText = validateUsername()
+        binding.passwordcontainer.helperText = validPassword()
+
+
+        val validEmail = binding.usernamecontainer.helperText == null
+        val validPassword = binding.passwordcontainer.helperText == null
+
+        if (validEmail && validPassword )
+            resetForm()
+        else
+            invalidForm()
+
+    }
+
+    private fun invalidForm() {
+        var message = ""
+        if(binding.usernamecontainer.helperText != null)
+            message += "\n\nEmail: " + binding.usernamecontainer.helperText
+        if(binding.passwordcontainer.helperText != null)
+            message += "\n\nPassword: " + binding.passwordcontainer.helperText
+
+
+        AlertDialog.Builder(this)
+            .setTitle("Invalid Form")
+            .setMessage(message)
+            .setPositiveButton("Okay"){ _,_ ->
+                // do nothing
+            }
+            .show()
+    }
+
+
+    private fun resetForm() {
+        var message = "Email: " + binding.username.text
+        message += "\nPassword: " + binding.Passwordlogin.text
+
+        AlertDialog.Builder(this)
+            .setTitle("Form submitted")
+            .setMessage(message)
+            .setPositiveButton("Okay"){ _,_ ->
+                binding.username.text = null
+                binding.Passwordlogin.text = null
+
+
+                binding.usernamecontainer.helperText = getString(R.string.required)
+                binding.passwordcontainer.helperText = getString(R.string.required)
+
+            }
+            .show()
+    }
+
+    fun usernameFocusChanger(){
+        binding.username.setOnFocusChangeListener{_, focused ->
+            if (!focused){
+                binding.usernamecontainer.helperText = validateUsername()
+            }
+        }
+    }
+
+private  fun validateUsername() : String?{
+
+    val username = binding.username.text.toString()
+
+    if (!Patterns.EMAIL_ADDRESS.matcher(username).matches())
+    {
+        return "invalide username"
+    }
+
+    return null
+}
+
+    private fun passwordFocusListener()
+    {
+        binding.Passwordlogin.setOnFocusChangeListener { _, focused ->
+            if(!focused)
+            {
+                binding.passwordcontainer.helperText = validPassword()
+            }
+        }
+    }
+
+    private fun validPassword(): String? {
+
+        val passwordText = binding.Passwordlogin.text.toString()
+        if(passwordText.length < 8)
+        {
+            return "Minimum 8 Character Password"
+        }
+        if(!passwordText.matches(".*[A-Z].*".toRegex()))
+        {
+            return "Must Contain 1 Upper-case Character"
+        }
+        if(!passwordText.matches(".*[a-z].*".toRegex()))
+        {
+            return "Must Contain 1 Lower-case Character"
+        }
+        if(!passwordText.matches(".*[@#\$%^&+=].*".toRegex()))
+        {
+            return "Must Contain 1 Special Character (@#\$%^&+=)"
+        }
+
+        return null
+
+    }
+
+
 
 
     fun gotoSign(v: View) {
@@ -27,8 +143,8 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    fun gotoHome(v: View) {
-        val intent = Intent(this, MainScreen::class.java)
-        startActivity(intent)
-    }
+//    fun gotoHome(v: View) {
+//        val intent = Intent(this, MainScreen::class.java)
+//        startActivity(intent)
+//    }
 }
--- app/src/main/java/ui/SignUp.kt
+++ app/src/main/java/ui/SignUp.kt
@@ -3,164 +3,82 @@ package ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
+import android.util.Patterns
 import android.view.View
-import com.example.dateforsure.R
-import com.example.dateforsure.databinding.ActivityMainBinding
+import com.example.dateforsure.databinding.SignUpBinding
 
 
 class SignUp : AppCompatActivity() {
 
- private  lateinit var binding: ActivityMainBinding
+    private lateinit var binding : SignUpBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = SignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setupListeners()
-
+        usernameFocusChanger()
+        EmailFocusChanger()
 
     }
 
-    fun goToVerification(v: View){
-        val intent = Intent(this, VerificationActivity::class.java)
-        startActivity(intent)
-    }
-
-    /**
-     * field must not be empy
-     */
-    private fun validateUserName(): Boolean {
-        if (binding.username.text.toString().trim().isEmpty()) {
-
-            binding.usernameContainer.error = "Required Field!"
-            binding.username.requestFocus()
-            return false
-        } else {
-            binding.usernameContainer.isErrorEnabled = false
-        }
-        return true
-    }
-
-
-    /**
-     * 1) field must not be empty
-     * 2) text should matches email address format
-     */
-    private fun validateUserEmail(): Boolean {
-        if (binding.useremail.text.toString().trim().isEmpty()) {
-            binding.useremailContainer.error = "Required Field!"
-            binding.useremail.requestFocus()
-            return false
-        } else if (!isValidUserEmail(binding.useremail.text.toString())) {
-            binding.emailTextInputLayout.error = "Invalid Email!"
-            binding.email.requestFocus()
-            return false
-        } else {
-            binding.useremailContainer.isErrorEnabled = false
-        }
-        return true
-    }
-
-    /**
-     * field must not be empy
-     */
-    private fun validateUsergender(): Boolean {
-        if (binding.usergender.text.toString().trim().isEmpty()) {
-
-            binding.usergenderContainer.error = "Required Field!"
-            binding.usergender.requestFocus()
-            return false
-        } else {
-            binding.usergenderContainer.isErrorEnabled = false
-        }
-        return true
-    }
-
-    /**
-     * 1) field must not be empty
-     * 2) password lenght must not be less than 6
-     * 3) password must contain at least one digit
-     * 4) password must contain atleast one upper and one lower case letter
-     * 5) password must contain atleast one special character.
-     */
-    private fun validatePassword(): Boolean {
-        if (binding.userpassword.text.toString().trim().isEmpty()) {
-            binding.userpasswordContainer.error = "Required Field!"
-            binding.userpassword.requestFocus()
-            return false
-        } else if (binding.userpassword.text.toString().length < 6) {
-            binding.userpasswordContainer.error = "password can't be less than 6"
-            binding.userpassword.requestFocus()
-            return false
-        } else if (!isStringContainNumber(binding.userpassword.text.toString())) {
-            binding.userpasswordContainer.error = "Required at least 1 digit"
-            binding.userpassword.requestFocus()
-            return false
-        } else if (!isStringLowerAndUpperCase(binding.userpassword.text.toString())) {
-            binding.userpasswordContainer.error =
-                "Password must contain upper and lower case letters"
-            binding.userpassword.requestFocus()
-            return false
-        } else if (!isStringContainSpecialCharacter(binding.userpassword.text.toString())) {
-            binding.userpasswordContainer.error = "1 special character required"
-            binding.userpassword.requestFocus()
-            return false
-        } else {
-            binding.userpasswordContainer.isErrorEnabled = false
-        }
-        return true
-    }
-
-
-    private fun setupListeners() {
-        binding.username.addTextChangedListener(TextFieldValidation(binding.username))
-        binding.useremail.addTextChangedListener(TextFieldValidation(binding.useremail))
-        binding.usergender.addTextChangedListener(TextFieldValidation(binding.usergender))
-        binding.userpassword.addTextChangedListener(TextFieldValidation(binding.userpassword))
-    }
-
-
-    /**
-     * applying text watcher on each text field
-     */
-    inner class TextFieldValidation(private val view: View) : TextWatcher {
-        override fun afterTextChanged(s: Editable?) {}
-        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            // checking ids of each text field and applying functions accordingly.
-            when (view.id) {
-                R.id.username -> {
-                    validateUserName()
-                }
-                R.id.userEmail -> {
-                    validateUserEmail()
-                }
-                R.id.userpassword -> {
-                    validatePassword()
-                }
-                R.id.usergender -> {
-                    validateUsergender()
-                }
+    fun usernameFocusChanger(){
+        binding.username.setOnFocusChangeListener{_, focused ->
+            if (!focused){
+                binding.usernameContainer.helperText = validateUsername()
             }
         }
     }
 
-}
+    private  fun validateUsername() : String?{
+
+        val usernameText = binding.username.text.toString()
+        if(usernameText.length < 8)
+        {
+            return "Minimum 8 Character Password"
+        }
+        if(!usernameText.matches(".*[A-Z].*".toRegex()))
+        {
+            return "Must Contain 1 Upper-case Character"
+        }
+        if(!usernameText.matches(".*[a-z].*".toRegex()))
+        {
+            return "Must Contain 1 Lower-case Character"
+        }
+        if(!usernameText.matches(".*[@#\$%^&+=].*".toRegex()))
+        {
+            return "Must Contain 1 Special Character (@#\$%^&+=)"
+        }
+
+        return null
+
+    }
 
 
+    fun EmailFocusChanger(){
+        binding.userEmail.setOnFocusChangeListener{_, focused ->
+            if (!focused){
+                binding.useremailContainer.helperText = validateUseremail()
+            }
+        }
+    }
 
-/**
- * applying text watcher on each text field
- */
-class TextFieldValidation(private val view: View) : TextWatcher {
-    override fun afterTextChanged(s: Editable?) {}
-    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-        // checking ids of each text field and applying functions accordingly.
+    private  fun validateUseremail() : String?{
 
+        val username = binding.userEmail.text.toString()
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(username).matches())
+        {
+            return "invalide Email"
+        }
+
+        return null
+    }
+
+
+    fun goToVerification(v: View){
+        val intent = Intent(this, VerificationActivity::class.java)
+        startActivity(intent)
     }
 
 }
